2ab5676898aefb1c5b7778f1cb96185727934732
ivan <ivanguerrero2305gmail.com>
2025-04-03 11:26:56 -0500

    signinphp

---
+++ src/signin.php
@@ -0,0 +1,35 @@
+<?php 
+      include ('../config/database.php');
+
+     $email = $_POST ['e_mail'];
+     $passw = $_POST ['p_sswd'];
+
+     $sql = "
+     SELECT 
+	    --id,
+	    --email,
+	    --password,
+	COUNT (id) as total
+	
+FROM users
+
+	WHERE 
+	email    =  '$email' and 
+	password =  '$passw' and
+	status = true 
+	GROUP BY 
+	id;
+
+     ";
+   $res = pg_query($conn, $sql);
+   
+    if ($res){
+        $row = pg_fetch_assoc ($res);
+        if($row['total'] > 0){
+            echo  "login ok";
+        }else {
+            echo "login fail";
+        }
+    }
+
+?>
